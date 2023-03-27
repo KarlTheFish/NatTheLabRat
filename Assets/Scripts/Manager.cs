bdf226b4e460f97d6186e97564718262c629d1cc
KarlTheFish <KarlTheFish@users.noreply.github.com>
2023-03-28 02:53:14 +0300

    added levels, added main menu, reset button works now, added standing rat asset

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Manager : MonoBehaviour
 {
     public MouseScript MouseScript;
     public GameObject Mouse;
+    public static int level;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +17,7 @@ public class Manager : MonoBehaviour
         Debug.Log(Mouse);
         MouseScript = Mouse.GetComponent<MouseScript>();
         Debug.Log(MouseScript);
+        level = 1;
     }
 
     // Update is called once per frame
@@ -32,5 +35,11 @@ public class Manager : MonoBehaviour
         MouseScript.LeftGate.transform.position = MouseScript.LeftGate.GetComponent<GateOpen>().LeftGateOGpos1;
         MouseScript.RightGate.transform.position = MouseScript.RightGate.GetComponent<GateOpen>().RightGateOGpos1;
         Mouse.GetComponent<Animator>().enabled = false;
+    }
+
+    public void Play()
+    {
+        level = level + 1;
+        SceneManager.LoadScene(level);
     }
 }
